7dece06ac496f124c9f6c4e3e725c64750b356aa
TheCatta <guilherme.fs.db@gmail.com>
2022-10-18 20:26:28 -0300

    🎊 init: commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Card } from './components/Card'
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-Primary flex justify-center items-center">
+        <Card/>
+        
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        Primary: '#242424',
+        Secundary: '#2f2f2f'
+      },
+      borderColor: {
+        projGradient:
+          'linear-gradient(90deg, rgba(149,131,226,1) 0%, rgba(90,124,203,1) 35%, rgba(190,130,216,1) 100%)'
+      },
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    }
+  },
+  plugins: []
+}
