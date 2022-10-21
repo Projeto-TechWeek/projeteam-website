72a71a9fe8b38c88080724d42ae2c6c62cec7502
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2022-10-21 00:31:43 -0300

    :wrench: Edição de cor do gradient

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
-        Primary: '#242424',
-        Secundary: '#2f2f2f'
+        Primary: "#242424",
+        Secundary: "#2f2f2f",
       },
-      borderColor: {
+      backgroundImage: {
         projGradient:
-          'linear-gradient(90deg, rgba(149,131,226,1) 0%, rgba(90,124,203,1) 35%, rgba(190,130,216,1) 100%)'
+          "linear-gradient(90deg, rgba(149,131,226,1) 0%, rgba(90,124,203,1) 35%, rgba(190,130,216,1) 100%)",
       },
       fontFamily: {
-        sans: 'Inter, sans-serif'
-      }
-    }
+        sans: "Inter, sans-serif",
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
