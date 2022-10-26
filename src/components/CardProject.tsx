9f8a636121df451379b90eb9a86514f4eb8fe5b3
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2022-10-26 15:10:28 -0300

    :sparkles: Adiciona Botão Ver Mais

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./Button";
 import { Card } from "./Card";
 import { Heading } from "./Heading";
 import { Tag } from "./Tag";
@@ -6,22 +7,22 @@ import { Text } from "./Text";
 export function CardProject() {
   return (
     <>
-      <Card.Root className="bg-[#848484] bg-opacity-10 border-2">
+      <Card.Root className="bg-white border-zinc-500 bg-opacity-10 backdrop-blur-sm border-2 drop-shadow-lg">
         <div className="flex px-4">
           <div className="flex flex-col max-w-[400px]">
-            <Card.Header className="text-zinc-900 !justify-start">
-              <div className="h-[146px] w-[340px] rounded overflow-clip bg-gray-300">
+            <Card.Header className="!justify-start">
+              <div className="h-[146px] sm:w-[9em] md:w-[11em] lg:w-[22em] rounded overflow-clip bg-gray-300">
                 <img src="" />
               </div>
             </Card.Header>
             <Card.Content>
               <div className="flex gap-2 items-center mt-3">
-                <Heading className="text-zinc-900">Projeteam</Heading>
+                <Heading className="text-zinc-200">Projeteam</Heading>
                 <Tag label="Tecnologia" className="bg-cyan-400" />
               </div>
             </Card.Content>
             <Card.Footer>
-              <Text className="text-zinc-900 mt-3">
+              <Text className="text-zinc-200 mt-3 pr-6">
                 O Projeteam é um projeto do curso de Análise e Desenvolvimento
                 de Sistemas que visa a possibilitar a formação de grupos para
                 realizar projetos sociais, pessoais...
@@ -29,10 +30,13 @@ export function CardProject() {
             </Card.Footer>
           </div>
 
-          <div>
-            <Card.Root className="w-[170px] p-2">
+          <div className="flex flex-col">
+            <Card.Root className="w-[170px] p-2 bg-transparent flex border-2 border-zinc-500">
               <Card.Header>
-                <Heading size="sm" className="flex justify-center items-start">
+                <Heading
+                  size="sm"
+                  className="flex justify-center items-start text-zinc-200"
+                >
                   Vagas
                 </Heading>
               </Card.Header>
@@ -42,6 +46,9 @@ export function CardProject() {
                 <Tag label="Banco de dados | 4" className="bg-yellow-200" />
               </Card.Content>
             </Card.Root>
+            <Button className="mt-auto bg-gradient-to-r from-[#6b92ed] via-[#9583E2] to-[#BE82D8] transition-colors ease-linear delay-75 hover:from-[#da84ff] text-gray-50">
+              Ver mais
+            </Button>
           </div>
         </div>
       </Card.Root>
