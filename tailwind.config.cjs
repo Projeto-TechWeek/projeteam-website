21fd27b95deac7eee2e5f0c2a40a0a07e72accf0
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2022-10-26 15:09:49 -0300

    :wrench: Remove background gradient tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,16 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        Primary: "#242424",
+        Primary: "#1E1C22",
         Secundary: "#2f2f2f",
-      },
-      backgroundImage: {
-        projGradient:
-          "linear-gradient(90deg, rgba(149,131,226,1) 0%, rgba(90,124,203,1) 35%, rgba(190,130,216,1) 100%)",
       },
       fontFamily: {
         sans: "Inter, sans-serif",
       },
+      screens: { sm: { min: "140px" } },
     },
   },
   plugins: [],
