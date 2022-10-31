0c6008c28695973bfb399c798827f47a0999fb7c
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2022-10-31 01:13:28 -0300

    🔧 feat: adiciona cores

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         Primary: "#1E1C22",
         Secundary: "#2f2f2f",
+        blue: "#5A7CCB",
+        purple: "#9583E2",
+        pink: "#BE82D8",
       },
       fontFamily: {
         sans: "Inter, sans-serif",
